6a3df15401e5db42174c097dbd9ff07d79897092
honnnei <hannpop@gmail.com>
2020-10-19 21:25:52 +0200

    cleanup

--- app.js
+++ app.js
@@ -5,48 +5,30 @@ const menus = require('./routes/menus');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
-
 mongoose.connect('mongodb://localhost/Menus', {useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 mongoose.set('useFindAndModify', false);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 
+    app.use( bodyParser.json() );       // to support JSON-encoded bodies
+    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+      extended: true
+    }));
 
+    app.use('/menus', menus);
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error:  err
+      });
+    });
 
-
-app.use('/menus', menus);
-
-app.get('/', function(req, res) {
-    res.send('OK');
-});
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error:  err
-  });
-});
-
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.json({
-//     message: err.message,
-//     error:  err
-//   });
-
-// });
-
-
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
 
 });
 
