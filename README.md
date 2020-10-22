52a3ac87cc92241c2b50856a72717e03d1fdfd41
honnnei <hannpop@gmail.com>
2020-10-22 14:47:55 +0200

    coloring the letters of the name according to the color order using recursion

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Tech Stack
+API:
+- The Cocktaildb API https://www.thecocktaildb.com/
+Backend Server:
 - Node JS
 - Express JS
+Database:
 - MongoDB + Mongoose
-- The Cocktaildb API https://www.thecocktaildb.com/
+Frontend:
+- TypeScript 
+- React JS with Hooks (create-react-app --template typescript)
+- React Router
+UI:
+- Bootstrap 4.5
+
+
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 export default function Header() {
+
+    const colorArray = [
+        "text-primary",
+        "text-secondary",
+        "text-success",
+        "text-danger",
+        "text-warning",
+        "text-info"
+    ]
+
+    const colorIndex = (array: string[], index: number):number => {
+        if (index < array.length) {
+            return index;
+        }
+         return colorIndex(array, index - array.length);
+    }
+
+    console.log(colorIndex(colorArray, 10));
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Coctail Menu Builder</h1>
+                <h1 className="display-4">{"Cocktail Menu Builder".split("").map((letter, index) => (
+                    <span className={colorArray[colorIndex(colorArray, index)]}>{letter}</span>
+                ))}
+                </h1>
                 <p className="lead">Design a custom cocktail menu for your venue or event.</p>
             </div>
         </div>
